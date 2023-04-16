e80486fa79dd96a623e1ab5440f6fff1f7a7feb9
Valiec <valiec2019@gmail.com>
2023-04-16 14:33:48 -0400

    Error checking.

--- Counting.java
+++ Counting.java
@@ -3,10 +3,21 @@ public class Counting {
 	{
 		boolean isMax = false;
 		int max = -1; //infinity
+		if(args.length > 1)
+		{
+			System.err.println("error: too many args");
+			System.err.println("usage: counting [max]");
+			System.exit(1);
+		}
 		if(args.length > 0)
 		{
 			max = Integer.parseInt(args[0]);
 			isMax = true;
+		}
+		if(max < 0)
+		{
+			System.err.println("error: max must be positive");
+			System.exit(1);
 		}
 		int i = 1;
 		while(i <= max || !isMax)
